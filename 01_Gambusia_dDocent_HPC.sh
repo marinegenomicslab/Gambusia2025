d4ae26af67c250a7d2664a00310a407b7562ed15
atfields <andrew.fields.biologist@gmail.com>
2025-05-27 14:05:08 -0500

    Update 01_Gambusia_dDocent_HPC.sh

--- 01_Gambusia_dDocent_HPC.sh
+++ 01_Gambusia_dDocent_HPC.sh
@@ -13,7 +13,7 @@ done
 #Trimming
 {```{bash}```
 ls | while read i; do cd $i; cp -sf $WORK/slurm/trim_config.file .; cp -sf $WORK/Workspace/Gambusia/reference/reference.fasta .; cd ..; done
-ls | while read i; do while [ $(squeue | grep afields | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/dDocent_trimming.slurm; cd ..; done
+ls | while read i; do while [ $(squeue | grep trim | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/dDocent_trimming.slurm; cd ..; done
 }
 
 #Mapping
@@ -23,16 +23,16 @@ ls | while read i; do cd $i; cp -sf $WORK/Workspace/Gambusia/reference/reference
 ls | while read i; do cd $i; bwa index reference.fasta & cd ..; done
 
 #Looking for overlap
-ls | while read i; do while [ $(squeue | grep afields | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_trimmed_reads.slurm; cd ..; sleep 2; done
+ls | while read i; do while [ $(squeue | grep PEAR | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_trimmed_reads.slurm; cd ..; sleep 2; done
 
 #Mapping reads
-ls | while read i; do while [ $(squeue | grep afields | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_mapping.slurm; cd ..; sleep 2; done
+ls | while read i; do while [ $(squeue | grep PEAR | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_mapping.slurm; cd ..; sleep 2; done
 
 #Filtering bam files
-ls | while read i; do while [ $(squeue | grep afields | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_PE_filter.slurm; cd ..; sleep 2; done
+ls | while read i; do while [ $(squeue | grep PEAR | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_PE_filter.slurm; cd ..; sleep 2; done
 
 #Combining SE and PE reads
-ls | while read i; do while [ $(squeue | grep afields | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_combo.slurm; cd ..; sleep 2; done
+ls | while read i; do while [ $(squeue | grep PEAR | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_combo.slurm; cd ..; sleep 2; done
 }
 
 #Subsetting the reference for later hapotyping
@@ -57,13 +57,13 @@ ls | while read i; do echo "Processing $i"; cd $i; cp -s ../../pear_mapping/$i/*
 ls | while read i; do echo "Processing $i"; cd $i; cp -s ../../reference/ref_subset.fasta ./reference.fasta; samtools faidx reference.fasta; bwa index reference.fasta; cd ..; done
 
 #Mapping reads
-ls | while read i; do while [ $(squeue | grep afields | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_mapping.slurm; cd ..; sleep 2; done
+ls | while read i; do while [ $(squeue | grep PEAR | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_mapping.slurm; cd ..; sleep 2; done
 
 #Filtering bam files
-ls | while read i; do while [ $(squeue | grep afields | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_PE_filter.slurm; cd ..; sleep 2; done
+ls | while read i; do while [ $(squeue | grep PEAR | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_PE_filter.slurm; cd ..; sleep 2; done
 
 #Combining SE and PE reads
-ls | while read i; do while [ $(squeue | grep afields | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_combo.slurm; cd ..; sleep 2; done
+ls | while read i; do while [ $(squeue | grep PEAR | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/pear/PEAR_combo.slurm; cd ..; sleep 2; done
 }
 
 #SNP calling
@@ -94,7 +94,7 @@ ls -d *.node | while read i; do echo "checking $i"; cd $i; ls raw.*.vcf | while 
 ls -d *.node | while read i; do cd $i; sbatch $WORK/slurm/dDocent_basic_filters.slurm; cd ..; sleep 2; done
 
 #Combine all vcfs in each node
-ls -d *.node | while read i; do while [ $(squeue | grep afields | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/dDocent_combine_filter_node.slurm; cd ..; sleep 2; done
+ls -d *.node | while read i; do while [ $(squeue | grep dDocent | wc -l) -ge 8 ]; do sleep 60; done; cd $i; sbatch $WORK/slurm/dDocent_combine_filter_node.slurm; cd ..; sleep 2; done
 
 #Preparing to combine all the vcf files
 mkdir vcf && cd vcf
